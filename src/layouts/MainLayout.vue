e6456ba22196835bca694a15ecb645eabd8629ea
boid-com <john@boid.com>
2021-03-02 04:24:43 -0600

    branding

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,6 +3,7 @@
     q-header.bg-grey-9
       q-toolbar
         q-toolbar-title
+          q-img(src="/public/icons/favicon-128x128.png")
           div EOS PowerUp
     q-page-container 
       router-view
