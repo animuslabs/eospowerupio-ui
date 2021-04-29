f9acedc7049376a6cb6cdd472d5aae9440a6b51f
boid-com <john@boid.com>
2021-04-29 15:11:47 -0500

    oops

--- src/lib/scatter.js
+++ src/lib/scatter.js
@@ -9,20 +9,20 @@ async function login() {
 
   try {
     ScatterJS.plugins(new ScatterEOS());
-    // const network = ScatterJS.Network.fromJson({
-    //   blockchain: 'eos',
-    //   chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
-    //   host: 'eos.greymass.com',
-    //   port: 443,
-    //   protocol: 'https'
-    // });
     const network = ScatterJS.Network.fromJson({
-      blockchain: 'kylin',
-      chainId: '5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191',
-      host: 'kylin.eosn.io',
+      blockchain: 'eos',
+      chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
+      host: 'eos.greymass.com',
       port: 443,
       protocol: 'https'
     });
+    // const network = ScatterJS.Network.fromJson({
+    //   blockchain: 'kylin',
+    //   chainId: '5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191',
+    //   host: 'kylin.eosn.io',
+    //   port: 443,
+    //   protocol: 'https'
+    // });
     const rpc = new JsonRpc(network.fullhost());
     const connection = await ScatterJS.connect('eospowerupio', { network })
     if (!connection) return console.error("No Scatter Connection")
